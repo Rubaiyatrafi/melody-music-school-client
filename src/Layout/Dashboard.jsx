a7be98dad9fa498923f326b1cc61bfa93e5dcf1e
Rubaiyatrafi <rubaiyatkabirrafi9166@gmail.com>
2023-06-13 13:17:47 +0600

    my classes dashboard page created

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { Link, Outlet } from "react-router-dom";
+import logo from "../assets/images/logo.png";
 import {
   FcLike,
   FcApproval,
@@ -8,41 +9,57 @@ import {
   FcReading,
   FcAssistant,
 } from "react-icons/fc";
+import useClasses from "../Hooks/UseClsasses";
 
 const Dashboard = () => {
+  const [selectedClasses] = useClasses();
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col items-center justify-center">
         {/* Page content here */}
-        <Outlet></Outlet>
+
         <label
           htmlFor="my-drawer-2"
-          className="btn btn-primary drawer-button lg:hidden"
+          className="btn btn-secondary drawer-button lg:hidden"
         >
-          Open drawer
+          Menu
         </label>
+        <Outlet></Outlet>
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 h-full bg-gradient-to-r from-indigo-200 via-purple-300 to-pink-200 text-base-content">
           {/* Sidebar content here */}
+          <li className="flex flex-col items-center justify-center">
+            <div>
+              <img className="w-12 rounded-lg" src={logo} alt="" />
+            </div>
+            <div>
+              <span className=" text-lg font-bold text-red-500">
+                Melody Music School
+              </span>
+            </div>
+          </li>
           <li>
-            <Link>
+            <Link to="/dashboard/home">
               <FcHome className=" text-xl"></FcHome>
               <span className=" text-base font-semibold">My Home</span>
             </Link>
           </li>
           <li>
-            <Link>
+            <Link to="/dashboard/mySelectedClasses">
               <FcLike className=" text-xl"></FcLike>
               <span className=" text-base font-semibold">
-                My Selected Classes
+                My Selected Classes{" "}
+                <span className="badge badge-secondary">
+                  {selectedClasses?.length || 0}
+                </span>
               </span>
             </Link>
           </li>
           <li>
-            <Link>
+            <Link to="/dashboard/myEnrolledClasses">
               <FcApproval className=" text-xl"></FcApproval>
               <span className=" text-base font-semibold">
                 My Enrolled Classes
@@ -50,11 +67,11 @@ const Dashboard = () => {
             </Link>
           </li>
           <li>
-            <Link>
+            <Link to="/dashboard/myPayment">
               <FcCurrencyExchange className=" text-xl"></FcCurrencyExchange>
               <span className=" text-base font-semibold">Payment History</span>
             </Link>
-            <Link>
+            <Link to="/dashboard/myGallery">
               <FcGallery className=" text-xl"></FcGallery>
               <span className=" text-base font-semibold">My Gallery</span>
             </Link>
--- src/Pages/Dashboard/MySelectedClasses/MySelectedClasses.jsx
+++ src/Pages/Dashboard/MySelectedClasses/MySelectedClasses.jsx
@@ -1,7 +1,107 @@
+import { Helmet } from "react-helmet-async";
+import useClasses from "../../../Hooks/UseClsasses";
+import { FcDeleteDatabase } from "react-icons/fc";
+import Swal from "sweetalert2";
+
 const MySelectedClasses = () => {
+  const [selectedClasses, refetch] = useClasses();
+  // console.log(selectedClasses);
+  const total = selectedClasses.reduce((sum, item) => item.price + sum, 0);
+
+  const handleDelete = (row) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/selectedClasses/${row._id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire("Deleted!", "Your class has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
   return (
-    <div>
-      <h1>My Selected Classes</h1>
+    <div className=" w-full p-5">
+      <Helmet>
+        <title>Melody Music | My Classes</title>
+      </Helmet>
+      <div className=" flex flex-col items-center mb-20">
+        <h1 className=" text-purple-300 font-semibold">---At a Glance---</h1>
+
+        <h1 className=" text-purple-500 text-5xl font-extrabold">My Classes</h1>
+      </div>
+      <hr />
+      <div className=" font-bold text-xl flex justify-evenly items-center m-5">
+        <h1>My Selected Classes: {selectedClasses.length}</h1>
+        <h1>Total Price: {total}</h1>
+        <button className="btn btn-outline btn-success btn-sm">Pay</button>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead className=" bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-300">
+            <tr>
+              <th>#</th>
+              <th>Classes</th>
+              <th>Instructors </th>
+              <th>User Email</th>
+              <th>Price(Tk)</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody className=" bg-slate-200">
+            {selectedClasses.map((row, index) => (
+              <tr key={row._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={row.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{row.music_class_name}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <div>
+                    <div className="text-sm opacity-50">
+                      {row.instructor_name}
+                    </div>
+                  </div>
+                </td>
+                <td>{row.email}</td>
+                <td>{row.price}</td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(row)}
+                    className="btn btn-sm btn-ghost bg-red-500"
+                  >
+                    <FcDeleteDatabase className=" text-xl"></FcDeleteDatabase>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
